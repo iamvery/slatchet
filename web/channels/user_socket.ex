4a508193502cc9308c8052caaf3c33ec33646f00
Jay Hayes <ur@iamvery.com>
2016-07-14 14:47:58 -0500

    Configure app for heroku
    Following http://www.phoenixframework.org/docs/heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Slatchet.UserSocket do
   # channel "room:*", Slatchet.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
