ad335a2fc3dd963d1b23656b0f8c9ec00e1b1b03
Jay Hayes <ur@iamvery.com>
2016-07-14 11:42:40 -0500

    Mark active channel with JS

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,13 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+// mark active channel
+let activeChannel = document.querySelector('#active-channel').innerText;
+let channels = document.querySelectorAll('.channel');
+channels.forEach(channel => {
+  let name = channel.querySelector('[data-prop=name]').innerText;
+  if (name == activeChannel) {
+    channel.attributes.class.value = `${channel.attributes.class.value} active`;
+  }
+});
