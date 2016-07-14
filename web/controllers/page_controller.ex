0e1d2ca18c1d76e6838f07c9f3c1202af1410528
Jay Hayes <ur@iamvery.com>
2016-07-14 14:11:14 -0500

    Replace inline plugs with modules

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,46 +1,12 @@
 defmodule Slatchet.PageController do
   use Slatchet.Web, :controller
 
-  plug :channels_data
-  plug :channel_data
-  plug :messages_data
-  plug :your_data
+  plug YourData
+  plug ChannelsData
+  plug ChannelData
+  plug MessagesData
 
   def index(conn, _params) do
     render conn, "index.html"
-  end
-
-  defp channels_data(conn, _opts) do
-    merge_data conn, %{
-      channels: [
-        %{name: "lolwat", message_count: "42"},
-        %{name: "ohai", message_count: inspect(Message.count)},
-        %{name: "noice", message_count: "10,437"},
-      ],
-    }
-  end
-
-  defp channel_data(conn, _opts) do
-    merge_data conn, %{
-      channel: %{name: "ohai"},
-    }
-  end
-
-  defp messages_data(conn, _opts) do
-    merge_data conn, %{
-      messages: Message.all,
-    }
-  end
-
-  defp your_data(conn, _opts) do
-    merge_data conn, %{
-      you: %{name: get_session(conn, :you)},
-    }
-  end
-
-  # TODO move this to ratchet lib in some form
-  defp merge_data(conn, data) do
-    data = Map.merge(conn.assigns.data, data)
-    %{conn | assigns: %{conn.assigns | data: data}}
   end
 end
