e583b4dc0cf3fd8038fecc8179682d6578390eb6
Jay Hayes <ur@iamvery.com>
2016-07-14 12:48:19 -0500

    Add simple, in-memory Message model

---
+++ web/models/message.ex
@@ -0,0 +1,15 @@
+defmodule Message do
+  def start_link do
+    Agent.start_link(fn -> {0, []} end, name: __MODULE__)
+  end
+
+  def all do
+    Agent.get(__MODULE__, fn {_count, messages} -> messages end)
+  end
+
+  def count, do: length(all)
+
+  def add(message) do
+    Agent.update(__MODULE__, fn {count, messages} -> {count+1, [message|messages]} end)
+  end
+end
