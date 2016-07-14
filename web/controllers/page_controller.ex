eaa01075040d63777488938febb7708a254abe79
Jay Hayes <ur@iamvery.com>
2016-07-14 11:40:40 -0500

    Plug data for current channel

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,21 @@
 defmodule Slatchet.PageController do
   use Slatchet.Web, :controller
 
+  plug :channel_data
+
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  defp channel_data(conn, _opts) do
+    merge_data conn, %{
+      channel: %{name: "ohai"},
+    }
+  end
+
+  # TODO move this to ratchet lib in some form
+  defp merge_data(conn, data) do
+    data = Map.merge(conn.assigns.data, data)
+    %{conn | assigns: %{conn.assigns | data: data}}
   end
 end
