ada6f3b9cc02e5dc0aba75bb3c377187fbd5a0be
Jay Hayes <ur@iamvery.com>
2016-07-14 07:42:02 -0500

    Add chat client mockup

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Slatchet.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Slatchet"
   end
 end
