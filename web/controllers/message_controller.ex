8fd5753d1bd27241dc33d4dbe87b76f9c034a57f
Jay Hayes <ur@iamvery.com>
2016-07-19 15:14:37 -0500

    Create message in correct channel

--- web/controllers/message_controller.ex
+++ web/controllers/message_controller.ex
@@ -1,8 +1,8 @@
 defmodule Slatchet.MessageController do
   use Slatchet.Web, :controller
 
-  def create(conn, %{"message" => message_params}) do
-    Message.add("ohai", get_session(conn, :you), message_params["body"])
+  def create(conn, %{"channel_id" => channel, "message" => message_params}) do
+    Message.add(channel, get_session(conn, :you), message_params["body"])
     redirect conn, to: "/"
   end
 end
