c26135535a4703bea319755d149a2c2c6ec343fe
Jay Hayes <ur@iamvery.com>
2016-07-19 12:26:45 -0500

    Plug data in channel controller

--- web/controllers/channel_controller.ex
+++ web/controllers/channel_controller.ex
@@ -1,6 +1,10 @@
 defmodule Slatchet.ChannelController do
   use Slatchet.Web, :controller
 
+  plug YourData
+  plug ChannelData
+  plug ChannelsData
+
   def show(conn, _params) do
     render conn, "show.html"
   end
